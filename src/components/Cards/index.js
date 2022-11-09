655d94c5d2545ce0913857ae2c73a939254a6ca9
SondosAliAraj <sondosali667@gmail.com>
2022-11-10 00:00:45 +0200

    pre-final commit: pass props to cards

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -3,28 +3,29 @@ import styles from "./style.module.css";
 import axios from "axios";
 import Card from "./Card";
 
-const Cards = ({ data, country }) => {
-  const [cdata, setData] = useState([]);
-  const [isFetch, setIsFetch] = useState([]);
-  const getData = async () => {
-    setIsFetch(true);
-    const response = await axios.get('https://covid19.mathdro.id/api');
-    // console.log(response, "res");
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }, country }) => {
 
-    setData(response.data);
-    setIsFetch(false);
-  };
+  // const [gdata, setData] = useState([]);
+  // const [isFetch, setIsFetch] = useState([]);
+  // const getData = async () => {
+  //   setIsFetch(true);
+  //   const response = await axios.get('https://covid19.mathdro.id/api');
+  //   // console.log(response, "res");
 
-  useEffect(() => {
-    getData();
-  }, []);
+  //   setData(response.gdata);
+  //   setIsFetch(false);
+  // };
 
-  if (isFetch) {
-    return 'Data is Loading ...';
-  }
-  console.log({ data });
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
-  if (!data.confirmed) {
+  // if (isFetch) {
+  //   return 'Data is Loading ...';
+  // }
+  // console.log({ data });
+
+  if (!confirmed) {
     return "loading...";
   }
     return (
@@ -33,8 +34,8 @@ const Cards = ({ data, country }) => {
           <div className={styles.infected}>
             <Card
               title="infected"
-              value={data.confirmed.value}
-              lastUpdate={data.lastUpdate}
+              value={confirmed.value} 
+              lastUpdate={lastUpdate}
               labelText="Number of infect cases of COVID-19"
               country={country}
             />
@@ -42,8 +43,8 @@ const Cards = ({ data, country }) => {
           <div className={styles.recovered}>
             <Card
               title="Recovered"
-              value={data.recovered.value}
-              lastUpdate={data.lastUpdate}
+              value={recovered.value}
+              lastUpdate={lastUpdate}
               labelText="Number of recoveries cases of COVID-19"
               country={country}
             />
@@ -51,8 +52,8 @@ const Cards = ({ data, country }) => {
           <div className={styles.deaths}>
             <Card
               title="deaths"
-              value={data.deaths.value}
-              lastUpdate={data.lastUpdate}
+              value={deaths.value}
+              lastUpdate={lastUpdate}
               labelText="Number of deaths cases of COVID-19"
               country={country}
             />
